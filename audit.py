f9956762cc520cf325e2abf3241400d4ae72fb23
Geenstack <hayleytops@gmail.com>
2022-02-02 21:31:20 +0300

    Изменил формат списка пакетов. Изменил вызываемый метод для vulners

--- audit.py
+++ audit.py
@@ -1,4 +1,14 @@
 import vulners
 from config import VULNERS_API_KEY
 
-vulners_api = vulners.Vulners(api_key=VULNERS_API_KEY)
+vulners_api = vulners.VulnersApi(api_key=VULNERS_API_KEY)
+
+'''
+Example audit packages vulns
+package_vulns = vulners_api.os_audit(os="debian", version="9", packages=["libpolkit-agent-1-0 0.105-18astra.se1 amd64"]))
+
+get CVE list
+package_vulns["cvelist"]
+['CVE-2019-6133', 'CVE-2018-19788', 'CVE-2021-4034']
+
+'''
